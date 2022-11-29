51b688a267ad4e05aef352682bd2fe7a752f3bd3
shpark0913 <shpark097754@gmail.com>
2022-11-29 18:53:38 +0900

    221129_JAVA 공부

--- 221129/chap06/src/sec03/exam01/Car.java
+++
@@ -1,8 +0,0 @@
-package sec03.exam01;
-
-public class Car {
-	//»ý¼ºÀÚ
-	Car(String color, int cc) {
-	}
-}
-
--- 221129/chap06/src/sec03/exam01/CarExample.java
+++
@@ -1,8 +0,0 @@
-package sec03.exam01;
-
-public class CarExample {
-	public static void main(String[] args) {
-		Car myCar = new Car("∞À¡§", 3000);
-		//Car myCar = new Car();  (x)
-	}
-}
