844c2e2b59d2449f8f257fb359947b6fde339a90
shpark0913 <shpark097754@gmail.com>
2022-12-12 21:02:07 +0900

    221212_JAVA 생성자 관련 작성 코드들

---
+++ 혼공자(혼자 공부하는 자바)/chap06/src/sec02/prob02/prob03.java
@@ -0,0 +1,13 @@
+package sec02.prob02;
+
+public class prob03 {
+
+	public static void main(String[] args) {
+
+		Member member = new Member();
+		member.name = "최하얀";
+		member.age = 23;
+		
+	}
+
+}
---
+++ 혼공자(혼자 공부하는 자바)/chap06/src/sec03/prob02/Member.java
@@ -0,0 +1,11 @@
+package sec03.prob02;
+
+public class Member {
+	String name;
+	String id;
+	
+	Member(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
