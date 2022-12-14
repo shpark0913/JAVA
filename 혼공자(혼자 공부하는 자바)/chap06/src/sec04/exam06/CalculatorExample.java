27075180ed929dd10a0a0400b82274c630d340af
shpark0913 <shpark097754@gmail.com>
2022-12-14 17:46:58 +0900

    221214_JAVA 메소드 공부

---
+++ 혼공자(혼자 공부하는 자바)/chap06/src/sec04/exam06/CalculatorExample.java
@@ -0,0 +1,21 @@
+package sec04.exam06;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+
+		Calculator myCalc = new Calculator();
+		
+		//정사각형의 넓이 구하기
+		double result1 = myCalc.areaRectangle(10);
+		
+		//직사각형의 넓이 구하기
+		double result2 = myCalc.areaRectangle(10, 20);
+		
+		//결과 출력
+		System.out.println("정사각형 넓이 : " + result1);
+		System.out.println("직사각형 넓이 : " + result2);
+		
+	}
+
+}
